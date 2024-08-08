4896de709cfb314e5dc5cdf0ced35f4b5e5404b2
Jirkby <akirkby24@gmail.com>
2024-08-08 13:45:04 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ function increment() {
     countEl.textContent = count
 }
 
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
 
